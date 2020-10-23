15756baa66d91f3ab96d474bd2f37e3d4aa1e06e
iswara.chaitanya <iswara@sivananda.org>
2020-10-23 18:25:12 -0400

    first dummy query

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,20 @@
-import express from "express";
+import express from 'express'
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { UserResolver } from './userResolvers'
 
-const app = express();
-app.get("/", (_req, res) => res.send("hello"));
-app.listen(4000, () => console.log("listening on port 4000."));
+const app = express()
+
+app.get('/', (_req, res) => res.send('hello'))
+
+async function start() {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [UserResolver] })
+  })
+
+  apolloServer.applyMiddleware({ app })
+  app.listen(4000, () => console.log('listening on port 4000.'))
+}
+
+start()
