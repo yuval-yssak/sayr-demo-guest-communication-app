b52d68fe3d93fe5f59ba3d42c7ec74b64465d51a
iswara.chaitanya <iswara@sivananda.org>
2020-10-24 13:29:43 -0400

    login and get access token

--- server/src/userResolvers.ts
+++ server/src/userResolvers.ts
@@ -7,12 +7,14 @@ import {
   Field,
   ID
 } from 'type-graphql'
-import { hash } from 'bcryptjs'
-import usersDAO from './dao/usersDAO'
+import { compare, hash } from 'bcryptjs'
 import { ObjectID } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import usersDAO from './dao/usersDAO'
+import { jwt } from '../config/config'
 
 @ObjectType()
-class User {
+class UserType {
   @Field(() => ID)
   _id: ObjectID
 
@@ -22,16 +24,17 @@ class User {
   password: string
 }
 
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
+
 @Resolver()
-export class UserResolver {
+class UserResolver {
   @Query(() => String)
   hello() {
     return 'Hello world!!!!'
-  }
-
-  @Query(() => [User])
-  async users() {
-    return await usersDAO.findArray({})
   }
 
   @Mutation(() => Boolean)
@@ -49,4 +52,26 @@ export class UserResolver {
     }
     return true
   }
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<LoginResponse> {
+    const user: UserType | undefined = (
+      await usersDAO.findArray({ email })
+    )?.[0]
+
+    if (!user) throw new Error('could not find user ' + email)
+
+    const valid: boolean = await compare(password, user.password)
+    if (!valid) throw new Error('wrong password')
+
+    const accessToken = sign({ userId: user._id }, jwt.secretKey, {
+      expiresIn: '15m'
+    })
+
+    return { accessToken }
+  }
 }
+
+export { UserResolver, UserType }
