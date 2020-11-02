adbac6a4f4ccf5f2021efb127d698f8f8777dbb4
iswara.chaitanya <iswara@sivananda.org>
2020-11-02 10:56:14 -0500

    use googleapis instead of axios for calendar requests

--- server/src/calendarResolver.ts
+++ server/src/calendarResolver.ts
@@ -1,7 +1,6 @@
 import { Resolver, Query, ObjectType, Field } from 'type-graphql'
 import usersDAO from './dao/usersDAO'
-import axios from 'axios'
-import { inspect } from 'util'
+import { calendar_v3, Auth } from 'googleapis'
 
 @ObjectType()
 class Event {
@@ -26,41 +25,37 @@ class CalendarResolver {
   @Query(() => [Event])
   async getEvents() {
     try {
-      const Authorization =
-        'Bearer ' +
-        (await usersDAO.findArray({ email: 'iswara@sivananda.org' }))?.[0].oauth
-          ?.google?.accessToken
-      console.log('auth token ', Authorization)
-      const response = await axios.get(
-        'https://www.googleapis.com/calendar/v3/calendars/iswara@sivananda.org/events',
-        { headers: { Authorization } }
-      )
+      const auth = new Auth.OAuth2Client()
+      auth.setCredentials({
+        access_token: (
+          await usersDAO.findArray({ email: 'iswara@sivananda.org' })
+        )?.[0].oauth?.google?.accessToken
+      })
 
-      console.log()
-      console.log()
-      console.log()
-      console.log(
-        inspect(response.data, { depth: Infinity, maxArrayLength: 5 })
-      )
-      console.log()
-      console.log()
+      const cal = new calendar_v3.Calendar({ auth })
+      const events = await cal.events.list({
+        calendarId: 'iswara@sivananda.org'
+      })
 
-      const events: Event[] = (<Array<any>>response.data.items)
-        .filter(event => event.summary && event.start && event.end)
-        // .filter((_, i) => i >= 18 && i < 25)
-        .map(event => {
-          console.log(
-            event.summary,
-            JSON.stringify(event.start),
-            JSON.stringify(event.end)
+      const filteredEvents =
+        events.data.items
+          ?.filter(
+            event =>
+              (event.start?.date || event.start?.dateTime) &&
+              (event.end?.date || event.end?.dateTime)
           )
-          return new Event(
-            event.summary,
-            new Date(event.start?.date || event.start?.dateTime),
-            new Date(event.end?.date || event.end?.dateTime)
-          )
-        })
-      return events
+          .map(
+            event =>
+              new Event(
+                event.summary || '',
+                new Date((event.start?.date || event.start?.dateTime)!),
+                new Date((event.end?.date || event.end?.dateTime)!)
+              )
+          ) || []
+
+      console.log('sync token  ', events.data.nextSyncToken)
+      console.log('page token  ', events.data.nextPageToken)
+      return filteredEvents
     } catch (e) {
       console.error(e)
       throw e
