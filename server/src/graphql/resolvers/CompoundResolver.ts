6c5e96595b216bad0be943b4ae577ad060885cfb
iswara.chaitanya <iswara@sivananda.org>
2020-12-13 15:43:27 -0500

    add arrivals query

--- server/src/graphql/resolvers/CompoundResolver.ts
+++ server/src/graphql/resolvers/CompoundResolver.ts
@@ -1,11 +1,14 @@
 import CompoundRegistrationsDAO, {
   IRegistration
 } from '../../dao/CompoundRegistrationsDAO'
-import { Resolver, Query } from 'type-graphql'
+import { Resolver, Query, Arg } from 'type-graphql'
 import RegistrationResponse from '../schema/RegistrationResponse'
 import * as compoundUtils from '../../services/compoundUtils'
 import dayjs from 'dayjs'
-import { inspect } from 'util'
+
+function today() {
+  return dayjs().format('YYYY-MM-DD')
+}
 
 @Resolver()
 export class CompoundResolver {
@@ -16,8 +19,8 @@ export class CompoundResolver {
       'persons.registrations': {
         $elemMatch: {
           status: { $ne: 'cancelled' },
-          start_date: { $lte: dayjs().format('YYYY-MM-DD') },
-          end_date: { $gte: dayjs().format('YYYY-MM-DD') },
+          start_date: { $lte: today() },
+          end_date: { $gte: today() },
           room: { $exists: true, $ne: '' }
         }
       }
@@ -33,8 +36,43 @@ export class CompoundResolver {
         reg =>
           reg.status !== 'cancelled' &&
           reg.room !== '' &&
-          reg.start_date <= dayjs().format('YYYY-MM-DD') &&
-          reg.end_date >= dayjs().format('YYYY-MM-DD')
+          reg.start_date <= today() &&
+          reg.end_date >= today()
+      )
+      .map(reg => new RegistrationResponse(reg))
+  }
+
+  @Query(() => [RegistrationResponse])
+  async upcomingArrivals(
+    @Arg('inUpcomingDays', { defaultValue: 2 }) inUpcomingDays: number
+  ): Promise<RegistrationResponse[]> {
+    console.log('arrivals in upcoming ' + inUpcomingDays)
+    const compounds = await CompoundRegistrationsDAO.findArray({
+      'persons.registrations': {
+        $elemMatch: {
+          status: { $ne: 'cancelled' },
+          start_date: {
+            $gte: today(),
+            $lte: dayjs().add(inUpcomingDays, 'day').format('YYYY-MM-DD')
+          },
+          room: { $exists: true, $ne: '' }
+        }
+      }
+    })
+
+    const allRegs = compounds.reduce(
+      (allRegs: IRegistration[], compound) =>
+        allRegs.concat(compoundUtils.getAllRegistrations(compound)),
+      []
+    )
+    return allRegs
+      .filter(
+        reg =>
+          reg.status !== 'cancelled' &&
+          reg.room !== '' &&
+          reg.start_date <=
+            dayjs().add(inUpcomingDays, 'day').format('YYYY-MM-DD') &&
+          reg.start_date >= today()
       )
       .map(reg => new RegistrationResponse(reg))
   }
