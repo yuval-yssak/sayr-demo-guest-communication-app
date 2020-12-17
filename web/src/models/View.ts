867a45ba0a7dffaff9be8b493a67545de0816c6c
iswara.chaitanya <iswara@sivananda.org>
2020-12-16 20:06:21 -0500

    refactor client

---
+++ web/src/models/View.ts
@@ -0,0 +1,83 @@
+import { Instance, types } from 'mobx-state-tree'
+import { match, compile } from 'path-to-regexp'
+
+export interface IView extends Instance<typeof View> {}
+
+type IPageWithId = { id: string }
+const viewModel = types
+  .model('View', {
+    page: types.union(
+      types.literal(''),
+      types.literal('/'),
+      types.literal('/login'),
+      types.literal('/register'),
+      types.literal('/secret'),
+      types.literal('/custom')
+    ),
+    id: types.maybe(types.string)
+  })
+  .views(self => ({
+    get currentURL() {
+      console.log('getting url', self.page)
+      switch (self.page) {
+        case '':
+        case '/':
+          return '/'
+        case '/custom':
+          const toPath = compile<IPageWithId>('/custom/:id')
+          return toPath({ id: self.id || '' })
+        default:
+          return self.page
+      }
+    }
+  }))
+
+  .actions(self => ({
+    openHomePage() {
+      self.page = '/'
+      self.id = undefined
+    },
+    openCustomPage(id: string) {
+      self.page = '/custom'
+      self.id = id
+    },
+    openLoginPage: () => (self.page = '/login'),
+    openRegisterPage: () => (self.page = '/register'),
+    openSecretPage: () => (self.page = '/secret'),
+    setFromURL() {
+      const newView = getViewFromURL() as Instance<typeof viewModel>
+      console.log('setting from url', window.location.pathname, newView.page)
+      self.page = newView.page
+      self.id = newView.id
+    }
+  }))
+
+export const View = types.optional(
+  viewModel,
+  getViewFromURL() as Instance<typeof viewModel>
+)
+
+function getViewFromURL() {
+  console.log('get view from url')
+
+  const { pathname } = window.location
+  const matchCustom = match<IPageWithId>('/custom/:id')
+  const matchedCustom = matchCustom(pathname)
+
+  if (matchedCustom) return { page: 'custom', id: matchedCustom.params.id }
+
+  const matchGeneral = match('/:page')
+  const matchedGeneral = matchGeneral(pathname)
+
+  if (matchedGeneral)
+    switch (matchedGeneral.path) {
+      case '':
+      case '/':
+        return { page: '/' }
+      case '/login':
+      case '/register':
+      default:
+        return { page: '/' }
+    }
+  else return { page: '/' }
+}
---
+++ web/src/onStart.ts
@@ -0,0 +1,71 @@
+import { RootStoreType } from './models'
+import { reaction, autorun } from 'mobx'
+import { GraphQLClient } from 'graphql-request'
+
+export default function onStart(
+  rootStore: RootStoreType,
+  gqlHttpClient: GraphQLClient
+) {
+  // in case the user has just finished an OAuth login,
+  // store the access token in the client and head to the homepage.
+  if (window.location.pathname === '/after-google-login')
+    rootStore.finishGoogleLogin()
+
+  // whenever the view changes - push to browser history
+  reaction(
+    () => rootStore.view.currentURL,
+    path => {
+      if (window.location.pathname !== path)
+        window.history.pushState(null, '', path)
+    }
+  )
+
+  // handle page load - react to URL
+  rootStore.view.setFromURL()
+  if (window.location.pathname !== rootStore.view.currentURL)
+    window.history.replaceState(null, '', rootStore.view.currentURL)
+
+  // react to user manually navigating through history
+  window.onpopstate = function historyChange(ev: PopStateEvent) {
+    if (ev.type === 'popstate') rootStore.view.setFromURL()
+  }
+
+  // update authentication header for GraphQL whenever the access token updates.
+  reaction(
+    () => rootStore.loggedInUser?.accessToken,
+    accessToken => {
+      gqlHttpClient.setHeaders({
+        authentication: accessToken ? `bearer ${accessToken}` : ''
+      })
+    }
+  )
+
+  // refresh token whenever it's 5 seconds away from being expired.
+  autorun(() => {
+    if (
+      !rootStore.loggedInUser?.isTokenValidWithMargin(5000) &&
+      rootStore.loggedInUser?.isOnline
+    )
+      rootStore.loggedInUser?.refreshToken()
+  })
+
+  // synchronize login and logout on all tabs
+  // relying on the localStorageMixin to update the "loggedInUser"
+  window.addEventListener('storage', (e: StorageEvent) => {
+    if (e.key === 'mst-gql-rootstore') {
+      const storageOldValue: RootStoreType = JSON.parse(e.oldValue || '{}')
+      const storageNewValue: RootStoreType = JSON.parse(e.newValue || '{}')
+
+      if (
+        (storageOldValue?.loggedInUser &&
+          !storageNewValue?.loggedInUser &&
+          rootStore.loggedInUser) ||
+        (!storageOldValue?.loggedInUser &&
+          storageNewValue?.loggedInUser &&
+          !rootStore.loggedInUser)
+      ) {
+        rootStore.setLoggedInUser(storageNewValue.loggedInUser)
+      }
+    }
+  })
+}
