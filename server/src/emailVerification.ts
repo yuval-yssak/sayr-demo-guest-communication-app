60e0ce9aa864c184bab8064dbbf2663309b66ac4
iswara.chaitanya <iswara@sivananda.org>
2020-12-21 01:48:15 -0500

    set confirmation email link url in environment variable

--- server/src/emailVerification.ts
+++ server/src/emailVerification.ts
@@ -13,7 +13,7 @@ async function requestEmailVerification(
     from: 'iswara@sivananda.org',
     subject: `Authenticate ${email} for Authentication Demo`,
     text: 'and easy to do anywhere, even with Node.js',
-    html: `<p>Welcome to the authentication demo.</p><a href="http://localhost:4000/verify-email/${requestID}">Click here to verify your email address</a>`
+	html: `<p>Welcome to the authentication demo.</p><a href="${process.env.SERVER_BASE_URL}/verify-email/${requestID}">Click here to verify your email address</a>`
   }
   await send(msg)
   console.log('Email sent')
